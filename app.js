16b1e0a868aff219b64ecd0569c7dae7915ea953
harshverma250202 <harshverma250202@gmail.com>
2021-08-28 02:59:25 +0530

    make it better

--- app.js
+++ app.js
@@ -17,7 +17,23 @@ app.use(express.json())
 
 app.use(require('./routers/auth'))
 app.use(require('./routers/posts'))
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
+
+
+if ( process.env.NODE_ENV == "production"){
+
+    app.use(express.static("client/build"));
+
+    const path = require("path");
+
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+    })
+
+
+}
 
 app.listen(PORT, () => {
     console.log(`Server running live at Localhost:${PORT} `)
